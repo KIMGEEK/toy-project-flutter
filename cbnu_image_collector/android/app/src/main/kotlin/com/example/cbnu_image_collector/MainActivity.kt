7ced1a61879932476d787ba7c6dd800b03e2321d
MinSeok Oh <minseok.dev@gmail.com>
2022-06-24 20:57:26 +0900

    Make cbnu_image_collector

---
+++ cbnu_image_collector/android/app/src/main/kotlin/com/example/cbnu_image_collector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cbnu_image_collector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ cbnu_image_collector/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyWidget());
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
