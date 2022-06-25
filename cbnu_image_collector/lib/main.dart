6534eeb5375d5c624fb4d566f68eaac37aa56853
minseok-oh <minseok.dev@gmail.com>
2022-06-25 11:37:50 +0900

    Do http test

--- cbnu_image_collector/lib/main.dart
+++ cbnu_image_collector/lib/main.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(MyWidget());
+void main() => runApp(MyApp());
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: HttpApp(),
+    );
+  }
+}
+
+class HttpApp extends StatefulWidget {
+  const HttpApp({Key? key}) : super(key: key);
+
+  @override
+  State<HttpApp> createState() => _HttpAppState();
+}
+
+class _HttpAppState extends State<HttpApp> {
+  String result = '';
+  List? data;
+
+  @override
+  initState() {
+    super.initState();
+    data = new List.empty(growable: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CBNU IMAGE COLLECTOR'),
+      ),
+      body: Container(child: Center(child: Text('$result'))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          getJSONData();
+        },
+        child: const Icon(Icons.file_download),
+      ),
+    );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v2/search/image?target=title&query=충북대';
+    var response = await http.get(Uri.parse(url),
+        headers: {"Authorization": "KakaoAK ef09948c786a2300b857b679bc8bc2ad"});
+
+    setState(() {
+      var dataConvertedToJSON = json.decode(response.body);
+      List result = dataConvertedToJSON['documents'];
+      data!.addAll(result);
+    });
+    return response.body;
   }
 }
